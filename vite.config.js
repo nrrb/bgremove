52eeb98ce98a36ca494f4825fed62e029ad23756
Nicholas Bennett <nicholasbennett555@gmail.com>
2026-05-19 22:41:46 -0400

    rearrange for ghpages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  root: 'src',
   plugins: [vue(), tailwindcss()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 })
